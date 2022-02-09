1a072c4cc9140d05dc1ba31af86698707b39c956
Bryce Borba <borbaesb@gmail.com>
2022-02-09 09:16:58 -0800

    added history view and a button that adds the current day to disposedOf

--- src/components/inventory/Property.js
+++ src/components/inventory/Property.js
@@ -3,22 +3,57 @@ import { useLocation } from "react-router-dom"
 
 
 
-export const Property = ({prop, deleteProp}) => {
+export const Property = ({prop, deleteProp, disposeProp}) => {
 
     const CurrentUser = parseInt(localStorage.getItem("pbg_user"))
     const Loc = useLocation()
 
     return (
             <>
+            {
+            Loc.pathname === "/history" && prop.disposedOf != "" ? 
             <p className="property">         
                         --- <br/>VIN:{prop.vin} <br/>
                         SECT: {prop.location?.name}<br/> 
                         Stored: {prop.storedDate} <br/>
-                        Status: {prop.onHold ? `On Hold` : `Cleared`}   
-            </p>
+                        Status: {prop.onHold ? `On Hold` : `Cleared`} <br />        
+                        Disposed: {prop.disposedOf === "" ? `No` : `Yes ( ${prop.disposedOf} )` }
+            </p> : ""
+            }
+            {
+            Loc.pathname === "/inventory" ? 
+            <p className="property">         
+                        --- <br/>VIN:{prop.vin} <br/>
+                        SECT: {prop.location?.name}<br/> 
+                        Stored: {prop.storedDate} <br/>
+                        Status: {prop.onHold ? `On Hold` : `Cleared`} <br />        
+                        Disposed: {prop.disposedOf === "" ? `No` : `Yes`}
+            </p> : ""
+            }
+            {
+            Loc.pathname === "/disposal" && prop.disposedOf === "" ? 
+            <p className="property">         
+                        --- <br/>VIN:{prop.vin} <br/>
+                        SECT: {prop.location?.name}<br/> 
+                        Stored: {prop.storedDate} <br/>
+                        Status: {prop.onHold ? `On Hold` : `Cleared`} <br />        
+                        Disposed: {prop.disposedOf === "" ? `No` : `Yes:`}
+            </p> : ""
+            }
+            
            { 
                 prop.userId === CurrentUser && Loc.pathname === "/inventory"  ? 
                     <button className="btn--propertyDelete" onClick={() => deleteProp(prop.id)} >Delete</button> 
+                : "" 
+            }
+           { 
+                prop.disposedOf != "" && Loc.pathname === "/history"  ? 
+                    <button className="btn--propertyDelete" onClick={() => deleteProp(prop.id)} >Delete</button> 
+                : "" 
+            }
+           { 
+                Loc.pathname === "/disposal" && prop.disposedOf === ""  ? 
+                    <button className="btn--propertyDisposed" onClick={() => disposeProp(prop.id)} >Mark Disposed</button> 
                 : "" 
             }
        
